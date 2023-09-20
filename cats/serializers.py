2cb6eaef22b1c1a920428c2410c836ba16c1248d
Михаил Ренжин <renzhin@gmail.com>
2023-09-20 08:21:39 +0300

    Добавить: HEX в поле цйвета, переопределение названия поля, выбор значения поля из списка

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,25 +1,50 @@
 import datetime as dt
 
+# import webcolors
 from rest_framework import serializers
 
-from .models import Achievement, AchievementCat, Cat, Owner
+from .models import Achievement, AchievementCat, Cat, Owner, CHOICES
 
 
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
+
+
+# class Hex2NameColor(serializers.Field):
+#     # При чтении данных ничего не меняем - просто возвращаем как есть
+#     def to_representation(self, value):
+#         return value
+
+#     # При записи код цвета конвертируется в его название
+#     def to_internal_value(self, data):
+#         # Доверяй, но проверяй
+#         try:
+#             # Если имя цвета существует, то конвертируем код в название
+#             data = webcolors.hex_to_name(data)
+#         except ValueError:
+#             # Иначе возвращаем ошибку
+#             raise serializers.ValidationError('Для этого цвета нет имени')
+#         # Возвращаем данные в новом формате
+#         return data
 
 
 class CatSerializer(serializers.ModelSerializer):
     # owner = serializers.StringRelatedField(read_only=True)
-    achievements = AchievementSerializer(many=True, required=False)  # Убрали read_only=True
+    achievements = AchievementSerializer(many=True, required=False)
+    # Убрали read_only=True
     age = serializers.SerializerMethodField()
+    # color = Hex2NameColor()
+    color = serializers.ChoiceField(choices=CHOICES)
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = (
+            'id', 'name', 'color', 'birth_year', 'owner', 'achievements', 'age'
+        )
 
     def create(self, validated_data):
         # Если в исходном запросе не было поля achievements
